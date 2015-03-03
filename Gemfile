60790c6c0951defd8801289f4375b2f7b5d6a422
Andrea Hernandez <YOUR EMAIL ADDRESS>
2015-03-02 20:35:43 -0600

    got the clock and greeting working

--- Gemfile
+++ Gemfile
@@ -32,7 +32,12 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+#Wayyyyy better than WeBrick 
+gem 'thin'
+
 group :development, :test do
+  # SIMMA DOWN NAH!
+  gem 'quiet_assets'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
+    @current_time = Time.now.to_formatted_s(:short)
+    @greet_message = case Time.now.hour
+    when 5..11 
+      "Goooood Mernin"
+    when 12..15 
+      "Good Afternoon My Queen"
+    when 16..19 
+      "Buenas Tardes"
+    when 20..23, 0..4 
+      "Boa Noite"
+    end
+
   end
 end
